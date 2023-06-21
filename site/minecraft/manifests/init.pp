fb605b8dadca91994e5d89b5604ac3fa463e51b7
stevenmoto <137202686+stevenmoto@users.noreply.github.com>
2023-06-21 08:59:09 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -13,7 +13,7 @@ class minecraft {
     ensure => file,
     content => 'eula=true',
   }
-  file {'/etc/systemd/system/minecraft.server':
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
